455a0531dd4562c11628dfa3fe4afa001686cdf3
daramsreya83@gmail.com <your.email@example.com>
2025-07-15 13:58:52 +0530

    Assigment

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+  const s1Length=s1.length;
+  const s2Length=s2.length;
+  for(let i=0;i<s1Length-s2Length;i++){
+    for(let j=s2Length;j<s1Length;j++){
+      let sub=s1.substring(i,j);
+      if(sub===s2){
+        return i;
+      }
+    }
+  }
+  return -1;
 }
 
 // Please do not change the code below
